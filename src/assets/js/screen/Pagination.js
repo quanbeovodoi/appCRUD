c8a779d82a2d897b2f850960d5f22bd1c8a8a147
Quanbodoi <quanbeovodoi@gmail.com>
2023-07-19 10:50:36 +0700

    [UPDATED] Update code

---
+++ src/assets/js/screen/Pagination.js
@@ -0,0 +1,33 @@
+const Pagination = {
+    render: async (children) => {
+        return `<div class="pagination">
+        <p>Số bản ghi</p>
+        <div class="filter">
+            <select name="" id="filter">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+            </select>
+        </div>
+        <p class="infoPageCurrent">
+            1-10 of 20
+        </p>
+        <div class="nav_page">
+            <button class="prev">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path></svg>
+            </button>
+            <button class="next">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path></svg>
+            </button>
+        </div>
+    </div>`
+    }
+}
+export default Pagination;
